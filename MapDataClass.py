b62f6d77866308230d7efafe41ff65701c71c587
Kristof Muhi <kristof.muhi@innobie.tech>
2024-03-05 22:41:33 +0100

    add json mapping for map loading

---
+++ MapDataClass.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from enum import Enum
+import json
+
+@dataclass
+class Direction(Enum):
+    NORTH = 1
+    SOUTH = 2
+    EAST = 3
+    WEST = 4
+
+@dataclass
+class WallData():    
+    x_pos: int
+    y_pos: int
+    direction: int
+    
+    def to_object(d):
+        return WallData(d['x_pos'], d['y_pos'], d['direction'])
