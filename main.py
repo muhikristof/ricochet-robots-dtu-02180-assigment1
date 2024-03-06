6ba1ea8d3997efd63f7b5972984a187a6559213e
Theodor Arsenij <ftri2001@gmail.com>
2024-03-06 18:33:27 +0100

    A little refactorin to make `RicochetRobotsUI` type accessible from AIInterface

--- main.py
+++ main.py
@@ -1,5 +1,69 @@
+from enum import Enum
+from AIInterface import AIInterface, dumb_ai_test
+from typing import List, Tuple
+from MapDataClass import WallData
+from UI import RicochetRobotsUI
+import json
 import tkinter as tk
-from ui import RicochetRobotMenu
+
+
+class RicochetRobotMenu:
+    class MapName(Enum):
+        DEFAULT = "default"
+        EASY = "easy"
+        MEDIUM = "medium"
+        HARD = "hard"
+
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry("300x300")
+        self.init_ui()
+
+    def init_ui(self):
+        tk.Button(
+            self.master,
+            text="Default map",
+            height="3",
+            command=lambda: self.start_game(self.MapName.DEFAULT),
+        ).pack(side="top", fill="x")
+        tk.Button(
+            self.master,
+            text="Easy map",
+            height="3",
+            command=lambda: self.start_game(self.MapName.EASY),
+        ).pack(side="top", fill="x")
+        tk.Button(
+            self.master,
+            text="Medium map",
+            height="3",
+            command=lambda: self.start_game(self.MapName.MEDIUM),
+        ).pack(side="top", fill="x")
+        tk.Button(
+            self.master,
+            text="Hard map",
+            height="3",
+            command=lambda: self.start_game(self.MapName.HARD),
+        ).pack(side="top", fill="x")
+
+    def start_game(self, map_name: MapName):
+        self.master.destroy()
+        run_ricochet_robots_ui(self.load_map(map_name))
+
+    def load_map(self, map_name: MapName) -> List[WallData]:
+        with open(str.format("maps/{}.json", map_name.value), "r") as f:
+            return json.loads(f.read(), object_hook=WallData.to_object)
+
+
+def run_ricochet_robots_ui(map_data):
+    root = tk.Tk()
+    root.title("Ricochet Robots")
+    game_ui = RicochetRobotsUI(root, map_data)
+    ai_interface = AIInterface(game_ui)
+
+    # Initialize the AI. This is just a dummy test that moves a robot to goal. But the interaction should be similar.
+    # When building the AI, replace with root.after(0, method to run AI, ai_interface)
+    root.after(1000, dumb_ai_test, ai_interface)
+    root.mainloop()
 
 
 def show_game_ui():
--- ui.py
+++ ui.py
@@ -1,60 +1,9 @@
-from enum import Enum
-import json
 from typing import List, Tuple
-from AIInterface import AIInterface, dumb_ai_test
+import tkinter as tk
 from Board import Board
 from MapDataClass import WallData
 from Robot import Robot
-import tkinter as tk
-
-from Wall import Wall, Direction
-
-
-class RicochetRobotMenu:
-    class MapName(Enum):
-        DEFAULT = "default"
-        EASY = "easy"
-        MEDIUM = "medium"
-        HARD = "hard"
-
-    def __init__(self, master):
-        self.master = master
-        self.master.geometry("300x300")
-        self.init_ui()
-
-    def init_ui(self):
-        tk.Button(
-            self.master,
-            text="Default map",
-            height="3",
-            command=lambda: self.start_game(self.MapName.DEFAULT),
-        ).pack(side="top", fill="x")
-        tk.Button(
-            self.master,
-            text="Easy map",
-            height="3",
-            command=lambda: self.start_game(self.MapName.EASY),
-        ).pack(side="top", fill="x")
-        tk.Button(
-            self.master,
-            text="Medium map",
-            height="3",
-            command=lambda: self.start_game(self.MapName.MEDIUM),
-        ).pack(side="top", fill="x")
-        tk.Button(
-            self.master,
-            text="Hard map",
-            height="3",
-            command=lambda: self.start_game(self.MapName.HARD),
-        ).pack(side="top", fill="x")
-
-    def start_game(self, map_name: MapName):
-        self.master.destroy()
-        run_ricochet_robots_ui(self.load_map(map_name))
-
-    def load_map(self, map_name: MapName) -> List[WallData]:
-        with open(str.format("maps/{}.json", map_name.value), "r") as f:
-            return json.loads(f.read(), object_hook=WallData.to_object)
+from Wall import Direction
 
 
 class RicochetRobotsUI:
@@ -161,15 +110,3 @@ class RicochetRobotsUI:
         self.master.bind("<Left>", lambda e: self.move(-1, 0))
         self.master.bind("<Right>", lambda e: self.move(1, 0))
         self.master.bind("<Key>", self.switch_robot)
-
-
-def run_ricochet_robots_ui(map_data):
-    root = tk.Tk()
-    root.title("Ricochet Robots")
-    game_ui = RicochetRobotsUI(root, map_data)
-    ai_interface = AIInterface(game_ui)
-
-    # Initialize the AI. This is just a dummy test that moves a robot to goal. But the interaction should be similar.
-    # When building the AI, replace with root.after(0, method to run AI, ai_interface)
-    root.after(1000, dumb_ai_test, ai_interface)
-    root.mainloop()
